cc4eb320baba0ea0eae0bdbe864232566d201bc0
k3nozsCs <tarakenzo3@gmail.com>
2026-02-09 10:55:07 +0700

    menambahkan fitur menyerang

--- DuelPenyihir/Program.cs
+++ DuelPenyihir/Program.cs
@@ -1,17 +1,53 @@
-﻿wizard wizarda = new wizard("Heru", 10, 100);
-wizard wizardb = new wizard("Joko", 12, 100);
+﻿wizard wizarda = new wizard("Heru", 10);
+wizard wizardb = new wizard("Joko", 12);
+string pilihan;
 
 Console.WriteLine("Permainan Dimulai\n");
 wizarda.showStat();
 wizardb.showStat();
 
-wizarda.attack(wizardb);
-wizardb.attack(wizarda);
-wizarda.attack(wizardb);
+while (wizarda.energi > 0 && wizardb.energi > 0)
+{
+    Console.WriteLine($"1. {wizarda.name} Menyerang {wizardb.name}");
+    Console.WriteLine($"2. {wizardb.name} Menyerang {wizarda.name}");
+    Console.WriteLine($"3. {wizarda.name} Melakukan Heal");
+    Console.WriteLine($"4. {wizardb.name} Melakukan Heal");
+
+    Console.Write("\nMasukkan Pilihan (1/2/3/4) : ");
+    pilihan = Console.ReadLine();
+
+    switch(pilihan)
+    {
+        case "1":
+            wizarda.attack(wizardb);
+            break;
+        case "2":
+            wizardb.attack(wizarda);
+            break;
+        case "3":
+            wizarda.heal();
+            break;
+        case "4":
+            wizardb.heal();
+            break;
+        default:
+            Console.WriteLine("Pilihan Tidak Valid\n");
+            break;
+    }
+}
 
 Console.WriteLine("Permainan Berakhir\n");
 wizarda.showStat();
 wizardb.showStat();
+
+if (wizarda.energi >wizardb.energi)
+{
+    Console.WriteLine($"{wizarda.name} Menang!");
+}
+else
+{
+    Console.WriteLine($"{wizardb.name} Menang!");
+}
 
 public class wizard
 {
@@ -21,11 +57,11 @@ public class wizard
     public int damage;
 
     //constructor
-    public wizard(string name, int damage, int energi)
+    public wizard(string nama, int Damage)
     {
-        this.name = name;       
-        this.damage = damage;
-        this.energi = energi;
+       name = nama;
+        damage = Damage;
+        energi = 50;
     }
 
     public void showStat()
